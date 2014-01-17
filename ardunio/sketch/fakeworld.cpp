14a30999444afa0c30d59f0e392aa69f0c0e4fbe
Kenzie Togami <ket1999@gmail.com>
2014-01-16 21:21:52 -0800

    Attempt fake store impl

--- ardunio/sketch/fakeworld.cpp
+++ ardunio/sketch/fakeworld.cpp
@@ -1,1 +1,21 @@
 #include <Arduino.h>
+#include "fakeworld.h"
+
+// ref'd by first 2 bits for addr1 (0, 1, 10, 11)
+// and last 3 for addr 2 (0,1,10,11,100,101,110,111)
+byte fakebits[4];
+
+const int firstbits = bit(0) + bit(1);
+const int secondbits = bit(2) + bit(3) + bit(4);
+
+byte readfake(int addr) {
+  int index = addr & firstbits, index2 = (addr & secondbits) >> 2;
+  byte val = fakebits[index];
+  return (val >> index2) & 1;
+}
+
+void writefake(byte bit_, int addr) {
+  int index = addr & firstbits, index2 = (addr & secondbits) >> 2;
+  byte pre = fakebits[index];
+  fakebits[index] = (bit_ == 1) ? pre | (1 << index2) : pre & ~(1 << index2);
+}
