4a43e2ad584a1a8ecf4f8859b0d184c5ddadb323
Kenzie Togami <ket1999@gmail.com>
2014-01-16 16:36:33 -0800

    impl readbit, add read/write byte

--- ardunio/sketch/sketch.ino
+++ ardunio/sketch/sketch.ino
@@ -6,9 +6,23 @@ void loop() {
   
 }
 
+byte readbyte() {
+  byte b = 0;
+  for (int i = 0; i < 8; i++) {
+    b |= (readbit() << i);
+  }
+  return b;
+}
+
+void writebyte(byte b) {
+  for (int i = 0; i < 8; i++) {
+    writebit(b & bit(i));
+  }
+}
+
 /* Byte is smallest number, booleans are actually bytes */
 byte readbit() {
-  
+  return 0; // placeholder
 }
 
 void writebit(byte b) {
