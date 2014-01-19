d74569793257596bd35d014268c4da92303ca974
Kenzie Togami <ket1999@gmail.com>
2014-01-18 20:36:31 -0800

    Writes random, reads the parts.

--- ardunio/sketch/sketch.ino
+++ ardunio/sketch/sketch.ino
@@ -6,15 +6,20 @@ void setup() {
   Serial.begin(9600);
   randomSeed(analogRead(0)); // seed randomness (iff A0 is unconnected, change if not)
   pinMode(pin, OUTPUT);
-  writebyte(32, addr);
+  writebyte(random(127), addr);
 }
 
 void loop() {
+  writebyte(random(127), addr);
+  String build = "";
+  build += String(readbyte(addr)) + "=";
   for (int i = 0; i < 8; i++) {
-    Serial.println(String(readbit(addr, i) == 1) + "<-" + String(i));
-    digitalWrite(pin, (readbit(addr, i) == 1) ? HIGH : LOW);
-    delay(1000);
-    digitalWrite(pin, LOW);
-    delay(1000);
+    build += String(readbit(addr, i) << i);
+    if(i != 7) {
+      build +="+";
+    }
   }
+  Serial.println(build);
+  delay(1500);
 }
+
